f012697112869a20a91cf89fb632e759e608ea5f
Henry Chiang <zoozxc456@gmail.com>
2026-07-11 07:34:33 +0800

    feat(domain): add AdminLoginMethod entity

---
+++ choicepie-backend/ChoicePie.Backend/ChoicePie.Backend.Domain.Tests/Aggregates/AdminAuthAccount/Entities/AdminLoginMethodTests.cs
@@ -0,0 +1,36 @@
+using ChoicePie.Backend.Domain.Aggregates.AdminAuthAccount.Entities;
+using ChoicePie.Backend.Domain.Aggregates.AdminAuthAccount.Enums;
+
+namespace ChoicePie.Backend.Domain.Tests.Aggregates.AdminAuthAccount.Entities;
+
+[TestFixture]
+public class AdminLoginMethodTests
+{
+    [Test]
+    public void CreateOriginal_GivenPasswordHash_WhenCalled_ThenReturnsOriginalLoginMethod()
+    {
+        var loginMethod = AdminLoginMethod.CreateOriginal("hashed-password");
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(loginMethod.Id, Is.Not.EqualTo(Guid.Empty));
+            Assert.That(loginMethod.Provider, Is.EqualTo(AdminLoginProvider.Original));
+            Assert.That(loginMethod.PasswordHash, Is.EqualTo("hashed-password"));
+            Assert.That(loginMethod.ProviderUserId, Is.Null);
+        });
+    }
+
+    [Test]
+    public void CreateExternal_GivenProviderAndProviderUserId_WhenCalled_ThenReturnsExternalLoginMethod()
+    {
+        var loginMethod = AdminLoginMethod.CreateExternal(AdminLoginProvider.Original, "external-subject-id");
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(loginMethod.Id, Is.Not.EqualTo(Guid.Empty));
+            Assert.That(loginMethod.Provider, Is.EqualTo(AdminLoginProvider.Original));
+            Assert.That(loginMethod.ProviderUserId, Is.EqualTo("external-subject-id"));
+            Assert.That(loginMethod.PasswordHash, Is.Null);
+        });
+    }
+}
