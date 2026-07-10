dc7b18ba90f04c1b75bb0a3b1f5af34978f74624
Henry Chiang <zoozxc456@gmail.com>
2026-07-11 07:04:26 +0800

    feat(domain): add AuthAccount specifications and repository interface

---
+++ choicepie-backend/ChoicePie.Backend/ChoicePie.Backend.Domain.Tests/Aggregates/AuthAccount/Specifications/AuthAccountByEmailSpecificationTests.cs
@@ -0,0 +1,32 @@
+using ChoicePie.Backend.Domain.Aggregates.AuthAccount.Specifications;
+using ChoicePie.Backend.Shared.Kernel.ValueObjects;
+using AuthAccountAggregate = ChoicePie.Backend.Domain.Aggregates.AuthAccount.AuthAccount;
+
+namespace ChoicePie.Backend.Domain.Tests.Aggregates.AuthAccount.Specifications;
+
+[TestFixture]
+public class AuthAccountByEmailSpecificationTests
+{
+    [Test]
+    public void ToExpression_GivenAuthAccountWithMatchingEmail_WhenCompiledAndInvoked_ThenReturnsTrue()
+    {
+        var email = Email.Create("host@example.com");
+        var authAccount = AuthAccountAggregate.Register(email, "hashed-password", Guid.NewGuid());
+        var specification = new AuthAccountByEmailSpecification(email);
+
+        var isMatch = specification.ToExpression().Compile()(authAccount);
+
+        Assert.That(isMatch, Is.True);
+    }
+
+    [Test]
+    public void ToExpression_GivenAuthAccountWithDifferentEmail_WhenCompiledAndInvoked_ThenReturnsFalse()
+    {
+        var authAccount = AuthAccountAggregate.Register(Email.Create("host@example.com"), "hashed-password", Guid.NewGuid());
+        var specification = new AuthAccountByEmailSpecification(Email.Create("someone-else@example.com"));
+
+        var isMatch = specification.ToExpression().Compile()(authAccount);
+
+        Assert.That(isMatch, Is.False);
+    }
+}
