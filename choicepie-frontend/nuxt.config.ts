2c5175c6f766a2b591193ffb34b356b679fc90b7
Henry Chiang <zoozxc456@gmail.com>
2026-07-13 03:47:42 +0800

    chore(dev-env): run dev server over HTTPS on custom domain

--- choicepie-frontend/nuxt.config.ts
+++ choicepie-frontend/nuxt.config.ts
@@ -1,13 +1,42 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@nuxt/eslint', '@nuxt/ui', '@nuxt/image', '@vueuse/motion/nuxt', '@vueuse/nuxt', '@pinia/nuxt', '@nuxt/fonts', '@nuxtjs/sitemap', 'pinia-plugin-persistedstate/nuxt', '@nuxtjs/i18n'],
+  modules: [
+    '@nuxt/eslint',
+    '@nuxt/ui',
+    '@nuxt/image',
+    '@vueuse/motion/nuxt',
+    '@vueuse/nuxt',
+    '@pinia/nuxt',
+    '@nuxt/fonts',
+    '@nuxtjs/sitemap',
+    'pinia-plugin-persistedstate/nuxt',
+    '@nuxtjs/i18n'
+  ],
   devtools: {
     enabled: true
   },
-  css: [
-    '~/assets/css/main.css'
-  ],
+  css: ['~/assets/css/main.css'],
+  runtimeConfig: {
+    public: {
+      apiBaseUrl: 'https://choicepie-dev-api.minjie.demo'
+    }
+  },
+  devServer: {
+    https: {
+      key: './cert/key.pem',
+      cert: './cert/cert.pem'
+    },
+    host: '0.0.0.0',
+    port: 3000
+  },
   compatibilityDate: '2025-07-15',
+  vite: {
+    server: {
+      warmup: {
+        clientFiles: ['./app/layouts/**/*.vue', './app/components/common/**/*.vue']
+      }
+    }
+  },
   fonts: {
     families: [
       { name: 'Outfit', provider: 'google', weights: [400, 500, 600, 700, 800] },
